93091bf5dc08d4e209ac0f1728282991e3f3f6db
Avi Banerjee <avi.banerjee164@gmail.com>
2016-12-05 12:00:02 -0800

    Added customized bootstrap elemetents

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
- root "pages#home"
-get "about" => "pages#about"
+ root "pages#home"  #use root_path to link back to home
+ get "about" => "pages#about" #creates an about_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
