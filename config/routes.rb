3418ba2a7db26cba2c6eb5f561eb8ee06fe86a7d
Avi Banerjee <avi.banerjee164@gmail.com>
2016-12-05 15:19:39 -0800

    Developed problem1 carousel

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
  root "pages#home"  #use root_path to link back to home
  get "about" => "pages#about" #creates an about_path
+ get "problem1" => "pages#problem1"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
